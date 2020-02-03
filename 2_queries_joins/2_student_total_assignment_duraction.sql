2dce968f4140a147edea001aecc8e2f9bcfe62f0
Chris Luksatrio <ranggaluksatrio@gmail.com>
2020-02-03 22:02:46 +0000

    Queries III exercise

--- 2_queries_joins/2_student_total_assignment_duraction.sql
+++ 2_queries_joins/2_student_total_assignment_duraction.sql
@@ -2,3 +2,10 @@ SELECT sum(assignment_submissions.duration) as total_duration
 FROM assignment_submissions
 JOIN students ON students.id = student_id 
 WHERE name='Ibrahim Schimmel';
+
+SELECT students.name as student, count(assignment_submissions.*) as total_submissions
+FROM assignment_submissions
+JOIN students ON students.id = student_id
+WHERE students.end_date IS NULL
+AND sum(assignment_submissions.*) < 100
+GROUP BY students.name;
